5e459ac18599f210f2e10fac2c72e39fa5c298f2
Ivan Pizhenko <11859904+IvanPizhenko@users.noreply.github.com>
2025-04-14 11:38:50 +0300

    Create placeholder.cpp

---
+++ src/placeholder.cpp
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025, Ivan Pizhenko. All rights reserved.
+// A placeholder file. DO NOT REMOVE.
+
+namespace stdx {
+
+const int STDX_VERSION = 1;
+
+} // namespace
